3c790e3e6f4513a3e88ebabc419de574ff9b9666
Narsimlu <narsimlu.atg@gmail.com>
2022-05-17 13:43:09 +0530

    added baseurl
    added baseurl

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-baseURL: "http://localhost:9000",
+baseURL: "https://diversity-processing-service1.azurewebsites.net/",// "http://localhost:9000",
 headers:{
     "Content-type":"application/json"
 }
